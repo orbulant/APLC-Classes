43bc5efde3ae69b50423520fd5f461f867d79708
Orbulant <personal@tankashing.com>
2022-01-10 10:12:42 +0800

    pure functions EXPLAINED

--- Source/com.orbulant.tutorial1/src/App.java
+++ Source/com.orbulant.tutorial1/src/App.java
@@ -2,14 +2,31 @@ import java.lang.String;
 import java.util.List;
 
 public class App {
-    public boolean findNemo(List<String> names) {
-        return names.contains("Nemo");
+
+    // Not so pure...
+    public boolean findNemoUnpure(List<String> names){
+        try {
+            return names.contains("Nemo");
+        } catch (Exception e) {
+            return false;
+        }
     }
 
+    // Purer
+    public boolean findNemo(List<String> names) {
+        try {
+            return names.contains("Nemo");
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-    //Somehow lecturer said this is more pure that the first example...
+    // Somehow lecturer said this is more pure that the first example...
     /**
-     * A purer function compared to above, which i disagree with... violates the concept of Pure Functions should be simple ideology.
+     * A purer function compared to above, .findAny() has ONE exception which is
+     * handled by .orElse(null).
+     * This makes it so that exception handling is done which returns appropriately.
+     * 
      * @param names
      * @return
      */
